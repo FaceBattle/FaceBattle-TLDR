cb307d625053c069c32c108833d9835f4da8852a
Luiz Fernando Cirigliano Villela <lf.villela@gmail.com>
2015-05-15 18:34:28 -0300

    adding adj matrix

--- tests.py
+++ tests.py
@@ -1,6 +1,8 @@
 __author__ = 'luizfernando2'
 
 import FacebookInterface
+import numpy as np
+
 # import Serializers
 from facepy import GraphAPI
 
@@ -21,11 +23,19 @@ id = '10153217214370446'
 post = FacebookInterface.get_fb_post(id, token)
 post.comments = FacebookInterface.get_fb_comments(id, token)
 
-people_set = set()
+people_set = post.unique_people_set()
+
+people_list = list(people_set)
+
+people_dict = {}
+for i, person in enumerate(people_list):
+    people_dict[str(person.id)] = 1
+
+adj_matrix = np.zeros((len(people_list), len(people_list)))
+
 for comment in post.comments:
-    people_set.add(comment.owner)
-
-    people_set.update(comment.likes)
-
-for person in people_set:
-    print(person.name)
+    i_pos = people_dict[str(comment.owner.id)]
+    for person in comment.likes:
+        j_pos = people_dict[str(person.id)]
+        adj_matrix[i_pos, j_pos] += 1
+        adj_matrix[j_pos, i_pos] += 1
