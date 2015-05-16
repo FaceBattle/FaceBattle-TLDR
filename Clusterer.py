32b14ad47fe7582b851319fbb95dec7a372dd0a8
Guilherme Andrade <gbuenoandrade@gmail.com>
2015-05-16 18:13:50 -0300

    clusterer2

--- Clusterer.py
+++ Clusterer.py
@@ -1,6 +1,10 @@
 
 import numpy as np
 import TretaMining as TM
+from mcl_clustering import mcl
+import agglomod
+
+the_adj_matrix = []
 
 def GetAdjMatrixAndPeopleList(post): #post.comments
     people_list = list(post.unique_people_set())
@@ -16,21 +20,29 @@ def GetAdjMatrixAndPeopleList(post): #post.comments
             adj_matrix[j_pos, i_pos] += 1
     for i in range(0, len(adj_matrix[3])):
         adj_matrix[i, i] = 1
+    the_adj_matrix = adj_matrix
     return adj_matrix, people_list
 
-
-def GetClusters(adj_matrix):
-    M, cluster = TM.mcl(adj_matrix, expand_factor=5, inflate_factor=3)
-    print(cluster)
+def GetClustersOp2(adjMatrix):
+    cluster = agglomod.getClustersDictionary(adjMatrix)
     TM.getFullGraph(cluster)
     cluster_list = []
     for a in cluster.values():
         if a not in cluster_list:
             cluster_list.append(a)
-    sorted(cluster_list)
-    cluster_list = list(reversed(cluster_list))
-    for a in cluster_list:
-        print(a)
+    cluster_list.sort(reverse=True)
+    return cluster_list
+
+def GetClusters(adj_matrix):
+
+    M, cluster = mcl(adj_matrix, expand_factor=5, inflate_factor=3)
+
+    TM.getFullGraph(cluster)
+    cluster_list = []
+    for a in cluster.values():
+        if a not in cluster_list:
+            cluster_list.append(a)
+    cluster_list.sort(reverse=True)
     return cluster_list
 
 def GetPeopleListAndClusterList(post): #post
@@ -38,4 +50,11 @@ def GetPeopleListAndClusterList(post): #post
     cluster_list = GetClusters(adj_matrix)
     return people_list, cluster_list
 
-
+def GetMostImportantPeople():
+    people = []
+    for i, people in enumerate(the_adj_matrix):
+        people.append(0)
+        for j, other_people in enumerate(the_adj_matrix[i]):
+            people[i] += the_adj_matrix[i][j]
+    people.sort(reverse=True)
+    return people
