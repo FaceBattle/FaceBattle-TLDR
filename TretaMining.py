1febad766277f8e06bd6f64bb86a18782d3d555c
Iago Almeida Neves <iagoneves9@gmail.com>
2015-05-16 15:46:16 -0300

    tretaming

--- TretaMining.py
+++ TretaMining.py
@@ -9,37 +9,19 @@ if __name__ == '__main__':
     app.run()
 '''
 
-#from graph_tool.all import *
+
 from mcl_clustering import mcl
 import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
-
-A = np.array(
-               [[1, 2, 3, 1, 0, 0],
-               [2, 1, 2, 1, 1, 0],
-               [3, 2, 1, 1, 0, 0],
-               [1, 1, 1, 1, 0, 0],
-               [0, 1, 0, 0, 1, 4],
-               [0, 0, 0, 0, 4, 1]])
-
-#M, clusters = mcl(A)
-#print(clusters)
-
-def getClusters(M):
-    return mcl(M)
 
 from networkx_viewer import Viewer
 
 def getCircularGraph(clusters):
     G = nx.Graph()
     for node, cluster in clusters.items():
-        if (len(cluster) > 1):
-            G.add_node(node)
-    for node, cluster in clusters.items():
-        if G.has_node(node):
+        if len(cluster) > 2:
             for i, each_node in enumerate(cluster):
-                if G.has_node(each_node):
                     if (i):
                         G.add_edge(each_node, prev)
                     else:
@@ -51,33 +33,17 @@ def getCircularGraph(clusters):
     print(len(G.edges()))
     nx.draw(G)
     plt.show()
-    #app = Viewer(G)
-    #app.mainloop()
 
 def getFullGraph(clusters):
     G = nx.Graph()
-    for node, cluster in clusters.items():
-        if (len(cluster) > 2):
-            G.add_node(node)
-    for node, cluster in clusters.items():
-        if G.has_node(node):
+    for id, cluster in clusters.items():
+        #if len(cluster) > 2:
             for a in cluster:
-                if G.has_node(a):
                     for b in cluster:
-                        if G.has_node(b):
                             if a != b:
                                 G.add_edge(a, b)
     nx.draw(G)
     print(len(G.nodes()))
     print(len(G.edges()))
-    #nx.draw_random(G)
     plt.show()
 
-
-
-# draw graph
-#nx.draw(G)
-#nx.draw_spring(G)
-#nx.draw_random(G)
-#nx.draw_circular(G)
-#nx.draw_spectral(G)
