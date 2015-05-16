4d862a28d0cc787886a850c57686e75539c912d8
Iago Almeida Neves <iagoneves9@gmail.com>
2015-05-16 12:17:41 -0300

    migue

--- tests.py
+++ tests.py
@@ -20,22 +20,42 @@ id = '10153217214370446'
 #     except StopIteration:
 #             break
 
-post = FacebookInterface.get_fb_post(id, token)
-post.comments = FacebookInterface.get_fb_comments(id, token)
+# post = FacebookInterface.get_fb_post(id, token)
+# post.comments = FacebookInterface.get_fb_comments(id, token)
+#
+# people_set = post.unique_people_set()
+#
+# people_list = list(people_set)
+#
+# people_dict = {}
+# for i, person in enumerate(people_list):
+#     people_dict[str(person.id)] = i
+#
+# adj_matrix = np.zeros((len(people_list), len(people_list)))
+#
+# for comment in post.comments:
+#     i_pos = people_dict[str(comment.owner.id)]
+#     for person in comment.likes:
+#         j_pos = people_dict[str(person.id)]
+#         adj_matrix[i_pos, j_pos] += 1
+#         adj_matrix[j_pos, i_pos] += 1
+#
 
-people_set = post.unique_people_set()
+#
 
-people_list = list(people_set)
 
-people_dict = {}
-for i, person in enumerate(people_list):
-    people_dict[str(person.id)] = i
+with open("tmp.bin", "rb") as file:
+    adj_matrix = np.load(file)
+    import TretaMining as TM
+    for i in range(0, len(adj_matrix[3])):
+        adj_matrix[i, i] = 1
+    M, cluster = TM.getClusters(adj_matrix)
+    print(cluster)
+    #TM.getFullGraph(cluster)
+    TM.getCircularGraph(cluster)
 
-adj_matrix = np.zeros((len(people_list), len(people_list)))
+# with open("tmp.bin", "wb") as file:
+#     np.save(file,adj_matrix)
 
-for comment in post.comments:
-    i_pos = people_dict[str(comment.owner.id)]
-    for person in comment.likes:
-        j_pos = people_dict[str(person.id)]
-        adj_matrix[i_pos, j_pos] += 1
-        adj_matrix[j_pos, i_pos] += 1
+#with open("tmp.bin", "rb") as file:
+#    adj_matrix = np.load(file)
