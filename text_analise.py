9bcbd74f9dd55c9cbc9cb1f5739bb29b0118887d
Luiz Fernando Cirigliano Villela <lf.villela@gmail.com>
2015-05-17 06:26:24 -0300

    rmeoving font

--- text_analise.py
+++ text_analise.py
@@ -91,7 +91,8 @@ def freq_comment(all_comments, _width = 200, _height = 100, save_file_name= 'tmp
     sorted_tokens_after = sorted_tokens_after[0:max_num_words]
 
     print("COMECANDO A PLOTAR WORDCLOUD")
-    wordcloud = WordCloud(width= _width,height= _height,font_path="Arial.ttf").generate_from_frequencies(sorted_tokens_after)
+    # wordcloud = WordCloud(width= _width,height= _height,font_path="Arial.ttf").generate_from_frequencies(sorted_tokens_after)
+    wordcloud = WordCloud(width= _width,height= _height).generate_from_frequencies(sorted_tokens_after)
 
     APP_ROOT = os.path.dirname(os.path.abspath(__file__))
     APP_STATIC = os.path.join(APP_ROOT, 'tmp')
